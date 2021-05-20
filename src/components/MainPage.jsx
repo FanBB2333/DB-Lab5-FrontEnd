1a97ff0afac1d0f989f4b2b2043a05f340d04cd5
FanBB2333 <2504082957@qq.com>
2021-05-20 23:27:10 +0800

    Change the style of MainPage

--- src/components/MainPage.jsx
+++ src/components/MainPage.jsx
@@ -17,16 +17,16 @@ const MainPage = () => {
     <>
     <Layout>
       <Content id='Page1'
-        style={{height: '580px'
+        style={{height: '675px'
       }}
       >
-        <br /><br /><br /><br />
+        <br /><br /><br /><br /><br /><br /><br />
         <div style={{top: '60%'}}>
           <p id='Title'>图书管理系统</p>
           {/* <Title /> */}
           <p id='Text'>Library Management</p>
         </div>
-        <Space style={{height: '450px'}}>
+        <Space style={{height: '320px'}}>
           <Button type="primary"><Link to="/login">管理员登录</Link></Button>
           <Button type="primary"><Link to="/query">查询</Link></Button>
         </Space>
@@ -37,7 +37,8 @@ const MainPage = () => {
         <div id='inFooter'>
           <div id='InfoBox'>
             <span>
-              <p className='Ftitle'>关于我</p>
+              <p className='Ftitle'>关于本项目</p>
+              <p >本项目仅用于数据库系统课程的Lab5验收哦</p>
 
             </span>
           </div>
